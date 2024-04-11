3fcf298eb7e57f5cd93fd0facfb8cb0c58b22250
lorenzo <lorenzo.mugnai5@stud.unifi.it>
2024-04-11 16:35:08 +0200

    Add SoA Operation

---
+++ cmake-build-debug/CMakeFiles/MidProjectMugnaiLorenzo.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lorenzo/CLionProjects/MidProjectMugnaiLorenzo/AoS/Manage.cpp" "CMakeFiles/MidProjectMugnaiLorenzo.dir/AoS/Manage.cpp.o" "gcc" "CMakeFiles/MidProjectMugnaiLorenzo.dir/AoS/Manage.cpp.o.d"
+  "/home/lorenzo/CLionProjects/MidProjectMugnaiLorenzo/AoS/ManageParallel.cpp" "CMakeFiles/MidProjectMugnaiLorenzo.dir/AoS/ManageParallel.cpp.o" "gcc" "CMakeFiles/MidProjectMugnaiLorenzo.dir/AoS/ManageParallel.cpp.o.d"
+  "/home/lorenzo/CLionProjects/MidProjectMugnaiLorenzo/AoS/Ngram.cpp" "CMakeFiles/MidProjectMugnaiLorenzo.dir/AoS/Ngram.cpp.o" "gcc" "CMakeFiles/MidProjectMugnaiLorenzo.dir/AoS/Ngram.cpp.o.d"
+  "/home/lorenzo/CLionProjects/MidProjectMugnaiLorenzo/AoS/ParallelAoS.cpp" "CMakeFiles/MidProjectMugnaiLorenzo.dir/AoS/ParallelAoS.cpp.o" "gcc" "CMakeFiles/MidProjectMugnaiLorenzo.dir/AoS/ParallelAoS.cpp.o.d"
+  "/home/lorenzo/CLionProjects/MidProjectMugnaiLorenzo/AoS/SequentialAoS.cpp" "CMakeFiles/MidProjectMugnaiLorenzo.dir/AoS/SequentialAoS.cpp.o" "gcc" "CMakeFiles/MidProjectMugnaiLorenzo.dir/AoS/SequentialAoS.cpp.o.d"
+  "/home/lorenzo/CLionProjects/MidProjectMugnaiLorenzo/SoA/ParallelSoA.cpp" "CMakeFiles/MidProjectMugnaiLorenzo.dir/SoA/ParallelSoA.cpp.o" "gcc" "CMakeFiles/MidProjectMugnaiLorenzo.dir/SoA/ParallelSoA.cpp.o.d"
+  "/home/lorenzo/CLionProjects/MidProjectMugnaiLorenzo/SoA/SequentialSoA.cpp" "CMakeFiles/MidProjectMugnaiLorenzo.dir/SoA/SequentialSoA.cpp.o" "gcc" "CMakeFiles/MidProjectMugnaiLorenzo.dir/SoA/SequentialSoA.cpp.o.d"
+  "/home/lorenzo/CLionProjects/MidProjectMugnaiLorenzo/main.cpp" "CMakeFiles/MidProjectMugnaiLorenzo.dir/main.cpp.o" "gcc" "CMakeFiles/MidProjectMugnaiLorenzo.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/MidProjectMugnaiLorenzo.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MidProjectMugnaiLorenzo.dir/AoS/Manage.cpp.o"
+  "CMakeFiles/MidProjectMugnaiLorenzo.dir/AoS/Manage.cpp.o.d"
+  "CMakeFiles/MidProjectMugnaiLorenzo.dir/AoS/ManageParallel.cpp.o"
+  "CMakeFiles/MidProjectMugnaiLorenzo.dir/AoS/ManageParallel.cpp.o.d"
+  "CMakeFiles/MidProjectMugnaiLorenzo.dir/AoS/Ngram.cpp.o"
+  "CMakeFiles/MidProjectMugnaiLorenzo.dir/AoS/Ngram.cpp.o.d"
+  "CMakeFiles/MidProjectMugnaiLorenzo.dir/AoS/ParallelAoS.cpp.o"
+  "CMakeFiles/MidProjectMugnaiLorenzo.dir/AoS/ParallelAoS.cpp.o.d"
+  "CMakeFiles/MidProjectMugnaiLorenzo.dir/AoS/SequentialAoS.cpp.o"
+  "CMakeFiles/MidProjectMugnaiLorenzo.dir/AoS/SequentialAoS.cpp.o.d"
+  "CMakeFiles/MidProjectMugnaiLorenzo.dir/SoA/ParallelSoA.cpp.o"
+  "CMakeFiles/MidProjectMugnaiLorenzo.dir/SoA/ParallelSoA.cpp.o.d"
+  "CMakeFiles/MidProjectMugnaiLorenzo.dir/SoA/SequentialSoA.cpp.o"
+  "CMakeFiles/MidProjectMugnaiLorenzo.dir/SoA/SequentialSoA.cpp.o.d"
+  "CMakeFiles/MidProjectMugnaiLorenzo.dir/main.cpp.o"
+  "CMakeFiles/MidProjectMugnaiLorenzo.dir/main.cpp.o.d"
+  "MidProjectMugnaiLorenzo"
+  "MidProjectMugnaiLorenzo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MidProjectMugnaiLorenzo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
